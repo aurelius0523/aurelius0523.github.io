ab73c3c4e72d10fe819d8637a981636882031505
aurelius0523 <aurelius0523@gmail.com>
2018-02-25 15:35:07 +0800

    testing cache control for google page insights

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,30 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $("#about_scroll").fadeOut();
   $("#work_scroll").fadeOut();
   $("#contact_scroll").fadeOut();
 
-  $("#career").click(function() {
+  $("#career").click(() => {
     $("#index").fadeOut();
     $("#about_scroll").fadeIn();
     $("#about_left").addClass("animated slideInLeft");
     $("#about_right").addClass("animated slideInRight");
   });
-  $("#technology").click(function() {
+
+  $("#technology").click(() => {
     $("#index").fadeOut();
     $("#work_scroll").fadeIn();
     $("#work_left").addClass("animated slideInLeft");
     $("#work_right").addClass("animated slideInRight");
   });
-  $("#contact").click(function() {
+
+  $("#contact").click(() => {
     $("#index").fadeOut();
     $("#contact_scroll").fadeIn();
     $("#contact_left").addClass("animated slideInLeft");
     $("#contact_right").addClass("animated slideInRight");
   });
 
-  $(".back").click(function() {
+  $(".back").click(() => {
     $(".pages").fadeOut();
     $("#index").fadeIn();
     $("#index_left").addClass("animated slideInLeft");
